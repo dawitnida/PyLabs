b5f8050865578041774f2ececcba7e2daec491da
Dawit Nida <dawitnida@users.noreply.github.com>
2014-09-11 19:25:53 +0300

    Exercise two: Decorator method implemented.

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,8 +1,10 @@
-""" Development of Web Applications and Web Services 
-    Assignment 1- Python
+''' Development of Web Applications and Web Services
+    Assignment 1- Python, Exercise 1
 
-    This program calculates the average number from a List and display result fetched from dictionary object
-"""
+    This program calculates the average number from a List and display result fetched from dictionary object,
+    If the average is less than 6, print 'Low Average', if average is between 6 and 12 print 'Medium Average' and
+    if average is greater than 12 print 'High Average'.
+'''
 
 __author__ = 'Dawit Nida (dawit.nida@abo.fi)'
 __date__ = '$Date: 2014-09-05 21:57:19 $'
@@ -10,11 +12,11 @@ __date__ = '$Date: 2014-09-05 21:57:19 $'
 
 class AverageCalculator():
     def __init__(self, numList = []):
-         self.nList = numList
+         self.numList = numList
     # calculate the average by adding the list elements and return a key for creating dictionary
     def calculate_average(self):
-        if self.nList:
-            average = sum(self.nList) / len(self.nList)      #two useful built-in methods 'sum' and 'len'
+        if self.numList:
+            average = sum(self.numList) / len(self.numList)      #two useful built-in methods 'sum' and 'len'
             if average <= 6:
                 return 'Low'
             elif 6 < average < 12:
@@ -22,30 +24,21 @@ class AverageCalculator():
             return 'High'
         return 'Empty'
     #to save the result and display, create a dictionary and assign to specific condition based on key<-> value
-    def create_dict(self, resultKey = ""):
-        self.resultKey = str(resultKey)
-        dict = {}
-        if resultKey == 'Low':
-            dict["Low"] = "Low Average"
-        elif resultKey == 'Medium':
-            dict["Medium"] = "Medium Average"
-        elif resultKey == 'High':
-            dict["High"] = "High Average"
-        else:
-            dict['Empty'] = "List is empty"
-        return dict
+    def display_message(self, dKey = ''):
+        dict = {'Low': 'Low Average', 'Medium': 'Medium Average', 'High': 'High Average', 'Empty': 'No list found'}
+        return dict[dKey]
 
 # lists of list for case study => output depends on the list
-list1 = [11,0,0,21]
+list0 = [-12]
+list1 = [1,0,0,1]
 list2 = [6,8]
 list3 = [10,12,14,21]
 list4 = []
 
 #pass the list when initializing the class object to get different results
-simpleCal = AverageCalculator(list2)
+simpleCal = AverageCalculator(list0)
 # retrieve the result from the created dictionary
-dictionaryResult = simpleCal.create_dict(simpleCal.calculate_average())
-print  dictionaryResult.get(simpleCal.calculate_average())      # display the value using 'get' method of dictionary
-
+dictionaryResult = simpleCal.display_message(simpleCal.calculate_average())
+print  dictionaryResult
 
 
